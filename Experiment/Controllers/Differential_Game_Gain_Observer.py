f65baf30f588e90784248e14d82144cebfad6aa5
lflipse <lorenzoflipse@gmail.com>
2021-07-07 11:31:37 +0200

    forgot to push lately

--- Experiment/Controllers/Differential_Game_Gain_Observer.py
+++ Experiment/Controllers/Differential_Game_Gain_Observer.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.linalg as cp
 import time
 
-class ControllerDG_GObs:
+class ControllerDG_GObsKal:
     def __init__(self, A, B, Gamma, Pi, kappa, Qr, Qh):
         self.A = A
         self.B = B
@@ -12,7 +12,6 @@ class ControllerDG_GObs:
         self.Qr = Qr
         self.Qh = Qh
         self.Lh_hat = np.array([0.0, 0.0])
-        print(A, B, Gamma, Pi)
 
     def compute_costs(self, x, u):
         return np.matmul(np.matmul(x.transpose(), self.Qr), x) + u**2
@@ -24,25 +23,28 @@ class ControllerDG_GObs:
         x_tilde = x_hat - x
         x_dot = states["state_derivative"]
         Lh_hat = states["estimated_human_gain"]
+        Qr = states["robot_cost"]
         uhhat = np.matmul(-Lh_hat, xi)
-        # Lh_hat = np.array([0, 0])
 
         # Compute Controller gain
         Acl = self.A - self.B * Lh_hat
         try:
-            Pr = cp.solve_continuous_are(Acl, self.B, self.Qr, 1)
+            Pr = cp.solve_continuous_are(Acl, self.B, Qr, 1)
         except:
             print("Debugs:")
+            print("Qr = ", Qr)
             print("Lhhat = ", Lh_hat)
-            exit("failed to find finite solution")
+            print("failed to find finite solution")
+            return -1
+            # exit("failed to find finite solution")
 
         Lr = np.matmul(self.B.transpose(), Pr)
         ur = np.matmul(-Lr, xi)
         Jr = self.compute_costs(xi, ur)
 
-        x_hat_dot = np.matmul(self.A, x) + self.B * (ur + uhhat + self.nonlinear_term(x))
+        x_hat_dot = np.matmul(self.A, x_hat) + self.B * (ur + uhhat + self.nonlinear_term(x)) - np.matmul(self.Pi, x_tilde)
         xi_tilde_dot = x_hat_dot - x_dot
-        uh_tilde = 1 / (np.matmul(self.B.transpose(), self.B)) * np.matmul(self.B.transpose(), xi_tilde_dot)
+        uh_tilde = (1 / (np.matmul(self.B.transpose(), self.B))) * np.matmul(self.B.transpose(), xi_tilde_dot - np.matmul((self.A - self.Pi), x_tilde))
         m_squared = 1 + self.kappa * np.matmul(xi.transpose(), xi)
         xi_gamma = np.matmul(xi.transpose(), self.Gamma)
         Lhhat_dot = uh_tilde * xi_gamma / m_squared
@@ -61,22 +63,21 @@ class ControllerDG_GObs:
         return output
 
     def nonlinear_term(self, x):
-        # Parameters
         g = 9.81
-        m = 0.4057759271653798
-        dh = 0.06279095432769782
-        dl = 0.009008157082978899
-        vt = 0.5217807023268454
+        m = 0.406
+        dh = 0.0628
+        dl = 0.00901
+        vt = 0.522
         vsp = 2 * vt
-        tau_fric = 0.0
-        tau_d = -0.08597506012132082
+        tau_d = -0.086
+        tau_fric = -0.0
 
         # Gravity
-        tau_g = - m * g * (dh * np.sin(x[0, 0]) + dl * np.cos(x[0, 0]))
+        tau_g = - m * g * dh * np.sin(x[0, 0]) - m * g * dl * np.cos(x[0, 0])
 
         # Friction
         v = x[1, 0]
-        gv = v / vsp * np.exp(-(v / (np.sqrt(2) * vsp)) ** 2 + (1 / 2))
-        tau_f = gv * tau_fric + tau_d * np.tanh(v / vt)
-        f_nl = tau_g + tau_f
-        return f_nl
+        gv = v / vsp * np.exp(-(v / (np.sqrt(2) * vsp)) ** 2 + 1 / 2)
+        fc = tau_d * np.tanh(v / vt)
+        tau_f = gv * tau_fric + fc
+        return tau_f + tau_g
